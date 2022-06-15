502bcb28caf4057ff65ab02cec390c0f03cd32e9
andrew sha <Xoma163rus@gmail.com>
2022-06-15 12:35:19 +0400

    renamed project to "yandex2mqtt"

--- apps/mqtt/apps.py
+++ apps/mqtt/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class YandexConfig(AppConfig):
+class MqttConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.yandex'
+    name = 'apps.mqtt'
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OpenhabYandex.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yandex2mqtt.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
